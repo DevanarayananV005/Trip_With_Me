71d05513c2ba02316840798e353352420c391679
DevanarayananV005 <78261863+DevanarayananV005@users.noreply.github.com>
2024-10-24 02:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2155,4 +2155,5 @@ def logout():
     return redirect(url_for('login')) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable if available, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port)
